6622601ef7cf3bafb74a9d3190d0328ed475bb71
ross-brown <rbrown_98@yahoo.com>
2023-09-13 19:05:22 -0500

    add error handling to item routes

--- app.js
+++ app.js
@@ -6,8 +6,22 @@ const { NotFoundError, BadRequestError } = require("./expressError");
 const itemRoutes = require("./itemRoutes");
 
 
-
 app.use(express.json());
+
 app.use("/items", itemRoutes);
+
+
+app.use(function (req, res) {
+  throw new NotFoundError();
+});
+
+
+app.use(function (err, req, res, next) {
+  const status = err.status || 500;
+  const message = err.message;
+  if (process.env.NODE_ENV !== "test") console.error(status, err.stack);
+  return res.status(status).json({ error: { message, status } });
+});
+
 
 module.exports = app;
--- itemRoutes.js
+++ itemRoutes.js
@@ -1,44 +1,77 @@
 const express = require("express");
-const {items} = require("./fakeDb");
+const { items } = require("./fakeDb");
 const router = new express.Router();
 
+const { NotFoundError, BadRequestError } = require("./expressError");
 
-router.get("/", function(req, res){
-  return res.json({items});
-})
-
+const ITEM_MODEL = { name: 'example', price: 'example' };
 
 
+router.get("/", function (req, res) {
+  return res.json({ items });
+});
 
-router.post("/", function(req, res){
-  //error handling?
+
+router.post("/", function (req, res) {
+
+  for (const key in req.body) {
+    if (!(key in ITEM_MODEL)) {
+      throw new BadRequestError("Invalid JSON sent.");
+    }
+  }
+
   const newItem = req.body;
+
+
+
   items.push(newItem);
-  return res.status(201).json({added: newItem});
-})
+  return res.status(201).json({ added: newItem });
+});
 
 
+router.get("/:name", function (req, res) {
+  if (!req.params.name) throw new BadRequestError();
 
-router.get("/:name", function(req, res){
   const item = items.find(item => item.name === req.params.name);
+
+  if (!item) throw new NotFoundError("Item doesn't exist.");
+
   return res.json(item);
-})
+});
 
 
-router.patch("/:name", function(req, res){
+router.patch("/:name", function (req, res) {
+  for (const key in req.body) {
+    if (!(key in ITEM_MODEL)) {
+      throw new BadRequestError();
+    }
+  }
+
+  const updatedInfo = req.body;
   const itemIndex = items.findIndex(item => item.name === req.params.name);
-  const newItem = req.body;
-  items[itemIndex] = newItem;
 
-  return res.json({updated: newItem});
-})
+  if (itemIndex === -1) throw new NotFoundError("Item doesn't exist.");
+
+  const item = items[itemIndex];
+
+  for (const key in updatedInfo) {
+    item[key] = updatedInfo[key];
+  }
+
+  return res.json({ updated: item });
+});
 
 
-router.delete("/:name", function(req, res){
+router.delete("/:name", function (req, res) {
+  if (!req.params.name) throw new BadRequestError();
+
   const itemIndex = items.findIndex(item => item.name === req.params.name);
+
+  if (itemIndex === -1) throw new NotFoundError("Item doesn't exist.");
+
   items.splice(itemIndex, 1);
 
-  return res.json({message: "Deleted"});
-})
+  return res.json({ message: "Deleted" });
+});
 
 module.exports = router;
